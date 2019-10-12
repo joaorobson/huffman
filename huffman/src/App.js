90d6568b328fa6de2a196ad5419f8104a6974fcb
joaorobson <joaorobsonmartins@gmail.com>
2019-10-11 23:04:59 -0300

    Read pixels and show image

--- huffman/src/App.js
+++ huffman/src/App.js
@@ -1,50 +1,63 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function  fileUpload(file, name) {
-    //const { sendMessage } = this.props;
-    //this.setState({ fileName: file.name });
-    //const formData = new FormData();
-    //formData.append("file", name);
-    const reader = new FileReader();
-    //const scope = this;
+function fileUpload(file, name) {
+  const reader = new FileReader();
 
-    reader.onload = e => {
+  var canvas = document.createElement("canvas"),
+    ctx = canvas.getContext("2d");
+  reader.onloadend = e => {
+    if (e.target.readyState == FileReader.DONE) {
       var content = e.target.result;
-      console.log(content)
-      var pixels = [];
-      var regex = /P6\n\d* \d*\n\d*\n(.*)/
-      var imageContent = regex.exec(content)[1]
-      var height = parseInt(content.split("\n").slice(1,2)[0].split(" ")[0]);
-      var width = parseInt(content.split("\n").slice(1,2)[0].split(" ")[1]);
-      console.log('ii', imageContent)
-      for(let i = 0; i < height*3; i += 1){
-        let aux = [];
-        let count = 0;
-        for(let j = 0; j < width*3; j += 1){
-          aux.push(imageContent.charCodeAt(i*height + j))
-          count++;
-          if(count === 3){
-            pixels.push(aux);
-            aux = []
-            count = 0;
-          }
-        }
-      }
-      console.log(pixels)
-    }
-    reader.readAsBinaryString(file);
 
-  }
+      console.log(content.length);
+      var regex = /P6\n\d* \d*\n\d*\n([\s\S]*)/;
+      var imageContent = regex.exec(content)[1];
+      var height = parseInt(
+        content
+          .split("\n")
+          .slice(1, 2)[0]
+          .split(" ")[0]
+      );
+      var width = parseInt(
+        content
+          .split("\n")
+          .slice(1, 2)[0]
+          .split(" ")[1]
+      );
+      canvas.width = width;
+      canvas.height = height;
+      var myImageData = ctx.createImageData(height, width);
+
+      var pixels = myImageData.data;
+      console.log(imageContent.length);
+      var j = 0;
+      for (let i = 0; i < pixels.length; i += 4) {
+        pixels[i] = imageContent.charCodeAt(j);
+        pixels[i + 1] = imageContent.charCodeAt(j + 1);
+        pixels[i + 2] = imageContent.charCodeAt(j + 2);
+        pixels[i + 3] = 255;
+        j += 3;
+      }
+      console.log(pixels);
+      ctx.putImageData(myImageData, 0, 0);
+
+      var image = new Image();
+
+      image.src = canvas.toDataURL();
+      document.body.appendChild(image);
+    }
+  };
+  reader.readAsBinaryString(file);
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <input
-            type="file"
-            onChange={e => fileUpload(e.target.files[0], 'ola')}
-          />
-
+          type="file"
+          onChange={e => fileUpload(e.target.files[0], "ola")}
+        />
       </header>
     </div>
   );
